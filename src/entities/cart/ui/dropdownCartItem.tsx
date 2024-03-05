695ed83b3a913bcc79c390c321f431fba182e830
dasha <darya.125viis.467@gmail.com>
2024-03-05 19:05:02 +0700

    replace default image

--- src/entities/cart/ui/dropdownCartItem.tsx
+++ src/entities/cart/ui/dropdownCartItem.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
-import defaultImage from 'shared/assets/images/defaultImage.jpg'
+import defaultImage from 'shared/assets/images/defaultImage.png'
 import { TPrice } from 'shared/types'
 import { Price } from 'shared/ui/price'
 
--- src/features/search/ui/dropdownSearchItem.tsx
+++ src/features/search/ui/dropdownSearchItem.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
-import defaultImage from 'shared/assets/images/defaultImage.jpg'
+import defaultImage from 'shared/assets/images/defaultImage.png'
 
 import './dropdownSearchItem.scss'
 
